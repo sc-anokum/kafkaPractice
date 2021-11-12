930547e002627c5e702dd39eea48f4fd25ac668e
sc-anokum <anoop.kumar@scryanalytics.com>
2021-11-12 12:46:41 +0530

    project updates

--- src/main/java/com/example/demo/repository/AlertRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Alert;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AlertRepository extends MongoRepository<Alert, String> {
-}
