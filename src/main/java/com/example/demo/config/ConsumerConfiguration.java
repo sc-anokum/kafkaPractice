1e614b524c763a614a0050b4e0029ceadac59ac0
sc-anokum <anoop.kumar@scryanalytics.com>
2021-11-16 16:41:19 +0530

    Added External Server Configuration

--- src/main/java/com/example/demo/config/ConsumerConfiguration.java
+++ src/main/java/com/example/demo/config/ConsumerConfiguration.java
@@ -1,6 +1,9 @@
 package com.example.demo.config;
 
 import com.example.demo.model.Alert;
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.config.SslConfigs;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -27,37 +30,60 @@ public class ConsumerConfiguration {
     @Value("${bootstrap.server}")
     private String BOOTSTRAP_ADDRESS;
 
-    @Bean
-    public ConsumerFactory<String, Alert> alertConsumerFactory(){
-        Map<String, Object> config = new HashMap<>();
-        config.put(org.apache.kafka.clients.consumer.ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_ADDRESS);
-        config.put(org.apache.kafka.clients.consumer.ConsumerConfig.GROUP_ID_CONFIG, ALERT_GROUP_ID);
-        config.put(org.apache.kafka.clients.consumer.ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        config.put(org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class.getName());
+    @Value("${ssl.keystore.location}")
+    private String KEYSTORE_LOCATION;
 
+    @Value("${ssl.keystore.password}")
+    private String KEYSTORE_PASSWORD;
+
+    @Value("${ssl.truststore.location}")
+    private String TRUSTSTORE_LOCATION;
+
+    @Value("${ssl.truststore.password}")
+    private String TRUSTORE_PASSWORD;
+
+    @Bean
+    public ConsumerFactory<String, Alert> alertConsumerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_ADDRESS);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, ALERT_GROUP_ID);
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class.getName());
+        config.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, KEYSTORE_LOCATION);
+        config.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, TRUSTSTORE_LOCATION);
+        config.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, KEYSTORE_PASSWORD);
+        config.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, TRUSTORE_PASSWORD);
+        config.put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, KEYSTORE_PASSWORD);
+        config.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL");
+        System.out.println(config);
         return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(), new JsonDeserializer<>(Alert.class));
     }
 
     @Bean(name = "alertListenerFactory")
-    public ConcurrentKafkaListenerContainerFactory<String, Alert> alertListenerContainerFactory(){
+    public ConcurrentKafkaListenerContainerFactory<String, Alert> alertListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, Alert> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(alertConsumerFactory());
         return factory;
     }
 
     @Bean
-    public ConsumerFactory<String, String> messageConsumerFactory(){
+    public ConsumerFactory<String, String> messageConsumerFactory() {
         Map<String, Object> config = new HashMap<>();
         config.put(org.apache.kafka.clients.consumer.ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_ADDRESS);
         config.put(org.apache.kafka.clients.consumer.ConsumerConfig.GROUP_ID_CONFIG, MESSAGE_GROUP_ID);
         config.put(org.apache.kafka.clients.consumer.ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-
+        config.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, KEYSTORE_LOCATION);
+        config.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, TRUSTSTORE_LOCATION);
+        config.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, KEYSTORE_PASSWORD);
+        config.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, TRUSTORE_PASSWORD);
+        config.put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, KEYSTORE_PASSWORD);
+        config.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL");
         return new DefaultKafkaConsumerFactory<>(config);
     }
 
     @Bean(name = "messageListenerFactory")
-    public ConcurrentKafkaListenerContainerFactory<String, String> messageListenerContainerFactory(){
+    public ConcurrentKafkaListenerContainerFactory<String, String> messageListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(messageConsumerFactory());
         return factory;
--- src/main/java/com/example/demo/config/ProducerConfiguration.java
+++ src/main/java/com/example/demo/config/ProducerConfiguration.java
@@ -1,6 +1,8 @@
 package com.example.demo.config;
 
 import com.example.demo.model.Alert;
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.common.config.SslConfigs;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -19,13 +21,30 @@ public class ProducerConfiguration {
     @Value("${bootstrap.server}")
     private String BOOTSTRAP_ADDRESS;
 
+    @Value("${ssl.keystore.location}")
+    private String KEYSTORE_LOCATION;
+
+    @Value("${ssl.keystore.password}")
+    private String KEYSTORE_PASSWORD;
+
+    @Value("${ssl.truststore.location}")
+    private String TRUSTSTORE_LOCATION;
+
+    @Value("${ssl.truststore.password}")
+    private String TRUSTORE_PASSWORD;
+
     @Bean
     public ProducerFactory<String, Alert> alertProducerFactory(){
         Map<String, Object> config = new HashMap<>();
         config.put(org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_ADDRESS);
         config.put(org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-
+        config.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, KEYSTORE_LOCATION);
+        config.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, TRUSTSTORE_LOCATION);
+        config.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, KEYSTORE_PASSWORD);
+        config.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, TRUSTORE_PASSWORD);
+        config.put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, KEYSTORE_PASSWORD);
+        config.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL");
         return new DefaultKafkaProducerFactory<>(config);
     }
 
@@ -41,7 +60,12 @@ public class ProducerConfiguration {
         config.put(org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_ADDRESS);
         config.put(org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-
+        config.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, KEYSTORE_LOCATION);
+        config.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, TRUSTSTORE_LOCATION);
+        config.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, KEYSTORE_PASSWORD);
+        config.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, TRUSTORE_PASSWORD);
+        config.put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, KEYSTORE_PASSWORD);
+        config.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL");
         return new DefaultKafkaProducerFactory<>(config);
     }
 
